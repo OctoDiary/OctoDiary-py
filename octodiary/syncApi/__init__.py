69a8238da566ade205fd9f5b1ab74c08851e5a8b
Den4ikSuperOstryyPer4ik <oleynik.denis.2009@gmail.com>
2023-09-21 16:01:56 +0300

    v0.2.4 - Удалён ASCII баннер, укорочен копирайт (`Copyright`)

--- octodiary/syncApi/__init__.py
+++ octodiary/syncApi/__init__.py
@@ -1,14 +1,5 @@
-#    ____       _        _____  _                  
-#   / __ \     | |      |  __ \(_)                 
-#  | |  | | ___| |_ ___ | |  | |_  __ _ _ __ _   _ 
-#  | |  | |/ __| __/ _ \| |  | | |/ _` | '__| | | |
-#  | |__| | (__| || (_) | |__| | | (_| | |  | |_| |
-#   \____/ \___|\__\___/|_____/|_|\__,_|_|   \__, |
-#                                             __/ |
-#                                            |___/ 
-# 
 #                 © Copyright 2023
-#        🔒 Licensed under the MIT License
+#          Licensed under the MIT License
 #        https://opensource.org/licenses/MIT
 #           https://github.com/OctoDiary
 
