49c6fac5615d8251e4af4e5507f38d5dad3e5734
Den4ikSuperOstryyPer4ik <oleynik.denis.2009@gmail.com>
2024-01-22 20:56:48 +0300

    [change] library version to 0.2.9

--- octodiary/__init__.py
+++ octodiary/__init__.py
@@ -3,7 +3,7 @@
 #        https://opensource.org/licenses/MIT
 #           https://github.com/OctoDiary
 
-__version__ = "0.2.8"
+__version__ = "0.2.9"
 
 from octodiary import asyncApi, exceptions, syncApi, types
 from octodiary.urls import MySchoolURLs
